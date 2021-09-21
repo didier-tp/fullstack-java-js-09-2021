3d86f63c716ed12037e1790084b9bc25af18c166
didierDefrance <didier@d-defrance.fr>
2021-09-21 17:35:44 +0200

    retour statut erreur

--- tp/appliJee/src/main/java/tp/appliJee/rest/CompteJsonRestCtrl.java
+++ tp/appliJee/src/main/java/tp/appliJee/rest/CompteJsonRestCtrl.java
@@ -1,8 +1,12 @@
 package tp.appliJee.rest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +27,26 @@ public class CompteJsonRestCtrl {
 	//methode qui retourne un seul compte de numero précisé
 	//RECHERCHE UNIQUE selon RESOURCE-ID:
 	//URL de déclenchement: .../appliJee/rest/compte/1
+	/*
 	@GetMapping(value="/{numCpt}" )
 	public Compte getCompteByNumero(@PathVariable("numCpt") Long numeroCpt) {
 	    return compteService.rechercherCompteParNumero(numeroCpt);
+	}
+	*/
+	
+	@GetMapping(value="/{numCpt}" ) 
+	public ResponseEntity<?> getCompteByNumero(@PathVariable("numCpt") Long numeroCpt) {
+	    Compte cpt = compteService.rechercherCompteParNumero(numeroCpt);
+	    if(cpt!=null) {
+	    	return new ResponseEntity<Compte>(cpt,HttpStatus.OK);
+	    }
+	    else {
+	    	Map<String,Object> mapError = new HashMap<>();
+	    	mapError.put("message","compte pas trouve");
+	    	mapError.put("numero de compte pas trouve",numeroCpt);
+	    	//return new ResponseEntity<String>( "{ \"message\" : \"compte pas trouve\" }",HttpStatus.NOT_FOUND);//404: NOT_FOUND
+	    	return new ResponseEntity<Map>( mapError,HttpStatus.NOT_FOUND);//404: NOT_FOUND
+	    }
 	}
 	
 	//méthode qui retourne plusieurs comptes (critere de recherche "numClient")
