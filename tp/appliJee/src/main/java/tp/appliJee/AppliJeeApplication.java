3e5004b60d21614a99a65b5b89950bde899bddf8
didierDefrance <didier@d-defrance.fr>
2021-09-22 12:58:05 +0200

    avec swagger

--- tp/appliJee/src/main/java/tp/appliJee/AppliJeeApplication.java
+++ tp/appliJee/src/main/java/tp/appliJee/AppliJeeApplication.java
@@ -10,7 +10,7 @@ public class AppliJeeApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(AppliJeeApplication.class, args);
 		SpringApplication app = new SpringApplication(AppliJeeApplication.class);
-		app.setAdditionalProfiles("mysql","reinit_dev");
+		app.setAdditionalProfiles("mysql","reinit_dev","swagger");
 		ConfigurableApplicationContext context = app.run(args);
 		
 		System.out.println("http://localhost:8080/appliJee");
