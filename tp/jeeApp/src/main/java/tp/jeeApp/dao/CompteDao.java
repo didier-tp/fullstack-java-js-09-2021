2234c80a53b45ca9c77b2f2cfeb237c3475dbe74
didierDefrance <didier@d-defrance.fr>
2022-01-24 14:46:00 +0100

    s

---
+++ tp/jeeApp/src/main/java/tp/jeeApp/dao/CompteDao.java
@@ -0,0 +1,17 @@
+package tp.jeeApp.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import tp.jeeApp.entity.Compte;
+
+//interface DAO avec méthodes CRUD (de memes noms que SpringData)
+
+public interface CompteDao {
+	Compte save(Compte entity); //saveOrUpdate
+	Optional<Compte> findById(Long numCpt);
+	//ou bien Compte findById(Long numCpt); si pas de spring ou jdk 1.7 ou 1.6
+	void deleteById(Long numCpt);
+	List<Compte> findAll();
+	//...
+}
---
+++ tp/jeeApp/src/main/java/tp/jeeApp/dao/CompteDaoImpl.java
@@ -0,0 +1,55 @@
+package tp.jeeApp.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import tp.jeeApp.entity.Compte;
+
+@Repository //@Repository hérite de @Component et signifie "DAO Spring"
+@Transactional()// propagation =  Propagation.REQUIRED par defaut
+public class CompteDaoImpl implements CompteDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public Compte save(Compte compte) {
+		if(compte.getNumero()==null) {
+			entityManager.persist(compte);
+		}
+		else {
+			compte = entityManager.merge(compte);
+		}
+		return compte;
+	}
+
+	@Override
+	public Optional<Compte> findById(Long numCpt) {
+		return Optional.ofNullable(entityManager.find(Compte.class, numCpt));
+	}
+
+	@Override
+	public void deleteById(Long numCpt) {
+		Compte cpt = entityManager.find(Compte.class, numCpt);
+		if(cpt != null) {
+			entityManager.remove(cpt);
+		}
+		else {
+			throw new RuntimeException("cannot delete Compte with numCpt="+numCpt);
+		}
+
+	}
+
+	@Override
+	public List<Compte> findAll() {
+		return entityManager.createQuery("SELECT c FROM Compte c",Compte.class)
+				            .getResultList();
+	}
+
+}
