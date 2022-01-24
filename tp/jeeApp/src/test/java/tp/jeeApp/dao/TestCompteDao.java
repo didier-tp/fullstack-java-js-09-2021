2467339000a93888977fb741ccf9e2f0a6620710
didierDefrance <didier@d-defrance.fr>
2022-01-24 17:33:41 +0100

    s

--- tp/jeeApp/src/test/java/tp/jeeApp/dao/TestCompteDao.java
+++ tp/jeeApp/src/test/java/tp/jeeApp/dao/TestCompteDao.java
@@ -27,10 +27,19 @@ public class TestCompteDao {
 	@Test
 	public void testBasic(){
 		Client client1 = new Client(null,"alex","Therieur");
-		Client client2 = new Client(null,"jean","Bon");
+		Client savedClient1 = clientDao.save(client1);
+		System.out.println("savedClient1="+savedClient1.toString());
+		//Assert.assertTrue(...) //en junit 4
+		Assertions.assertTrue(savedClient1 !=null); //en junit 5
 		
+		Client client2 = new Client(null,"jean","Bon");
+		Client savedClient2 = clientDao.save(client2);
+		System.out.println("savedClient2="+savedClient2.toString());
+		//Assert.assertTrue(...) //en junit 4
+		Assertions.assertTrue(savedClient2 !=null); //en junit 5
 		
 		Compte cptA  = new Compte(null,"compteA",100.0);
+		cptA.setProprietaire(savedClient1);
 		Compte savedCptA = compteDao.save(cptA);
 		System.out.println("savedCptA="+savedCptA.toString());
 		//Assert.assertTrue(...) //en junit 4
@@ -38,29 +47,25 @@ public class TestCompteDao {
 		
 		
 		Compte cptB  = new Compte(null,"compteB",50.0);
+		cptB.setProprietaire(savedClient1);
 		Compte savedCptB = compteDao.save(cptB);
 		System.out.println("savedCptB="+savedCptB.toString());
 		//Assert.assertTrue(...) //en junit 4
 		Assertions.assertTrue(savedCptB !=null); //en junit 5
 		
 		Compte cptC  = new Compte(null,"compteC",50.0);
+		cptC.setProprietaire(savedClient2);
 		Compte savedCptC = compteDao.save(cptC);
 		System.out.println("savedCptB="+savedCptC.toString());
 		//Assert.assertTrue(...) //en junit 4
 		Assertions.assertTrue(savedCptC !=null); //en junit 5
 		
 		
-		client1.addCompte(cptA); client1.addCompte(cptB);
-		Client savedClient1 = clientDao.save(client1);
-		System.out.println("savedClient1="+savedClient1.toString());
-		//Assert.assertTrue(...) //en junit 4
-		Assertions.assertTrue(savedClient1 !=null); //en junit 5
+		//client1.addCompte(cptA); client1.addCompte(cptB);
 		
-		client2.addCompte(cptC);
-		Client savedClient2 = clientDao.save(client2);
-		System.out.println("savedClient2="+savedClient2.toString());
-		//Assert.assertTrue(...) //en junit 4
-		Assertions.assertTrue(savedClient2 !=null); //en junit 5
+		
+		//client2.addCompte(cptC);
+		
 		
 		//List<Compte> comptes = compteDao.findAll();
 		List<Compte> comptes = compteDao.findComptesByClient(client1.getNumero());
