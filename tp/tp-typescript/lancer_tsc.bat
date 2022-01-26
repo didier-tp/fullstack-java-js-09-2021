cfd7db6a8137b7f29113f81a74181080bb1225e9
didierDefrance <didier@d-defrance.fr>
2022-01-26 15:37:57 +0100

    s

--- tp/tp-typescript/lancer_tsc.bat
+++ tp/tp-typescript/lancer_tsc.bat
@@ -1,2 +1,3 @@
-set PATH=C:\Users\d2fde\AppData\Roaming\npm;%PATH%
+REM set PATH=C:\Users\d2fde\AppData\Roaming\npm;%PATH%
+set PATH=C:\Users\d2fde\AppData\Roaming\npm\node_modules\typescript\bin;%PATH%
 tsc s1.ts
