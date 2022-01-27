66de7d7f2550e65e154f453ed313dcbf7389c856
didierDefrance <didier@d-defrance.fr>
2022-01-27 16:53:53 +0100

    s

--- tp/my-app/src/app/header/header.component.ts
+++ tp/my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +11,7 @@ export class HeaderComponent implements OnInit {
   @Input()
   titre :string = "titre_header"; //valeur par défaut
 
-  constructor() { 
+  constructor(public preferencesService : PreferencesService) { 
     console.log("dans constructor, titre="+this.titre);
   }
 
