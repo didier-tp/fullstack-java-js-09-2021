91560df2a66bb12dfe83f6ede13f7392fdc67850
didierDefrance <didier@d-defrance.fr>
2022-01-27 11:26:31 +0100

    tva

---
+++ tp/my-app/src/app/basic/tva/tva.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tva',
+  templateUrl: './tva.component.html',
+  styleUrls: ['./tva.component.scss']
+})
+export class TvaComponent implements OnInit {
+
+  ht : number =0;
+  taux : number = 20;//par defaut (en %)
+  listeTaux : number[]= [ 5 , 10 , 20]; //en %
+  tva : number =0;
+  ttc : number =0;
+  
+  onCalculerTvaEtTtc(){
+     this.tva = this.ht * this.taux / 100;
+     this.ttc = this.tva + this.ht;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
