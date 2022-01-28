d8541bccb5f5847d86792732f0cd9686be6b8659
didierDefrance <didier@d-defrance.fr>
2022-01-28 10:38:06 +0100

    s

--- tp/my-app/src/app/common/service/comptes.service.ts
+++ tp/my-app/src/app/common/service/comptes.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {  Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { Compte } from '../data/compte';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComptesService {
+
+  //valeurs en dur pour pré-version simulée.
+  private listeComptes : Compte[] = [
+    new Compte(1,"compteA",120.0),
+    new Compte(2,"compteB",50.0)
+  ];
+
+  rechercherComptesDuClient$(numClient :number) :Observable<Compte[]> {
+    return of(this.listeComptes)
+           .pipe(
+             delay(330) //simuler un temps d'attente de 330 ms
+           );
+  }
 
   constructor() { }
 }
--- tp/my-app/src/app/comptes/comptes.component.ts
+++ tp/my-app/src/app/comptes/comptes.component.ts
@@ -13,7 +13,17 @@ export class ComptesComponent implements OnInit {
   listeComptes : Compte[] = [];
 
   onRecupererComptes(){
-    //...
+    console.log("1. appel de rechercherComptesDuClient$() qui renvoi immédiatement un objet Observable")
+    if(this.numClient != undefined)
+       this.comptesService.rechercherComptesDuClient$(this.numClient)
+           .subscribe({
+             next: (comptes : Compte[])=>{ this.listeComptes = comptes; 
+                                  console.log("3. reponse récupérée en différé")},
+             error : (err)=>{console.log(err);}
+             }
+           );
+    console.log("2. suite éventuelle de onRecupererComptes sans attente")
+
   }
 
   constructor(private comptesService : ComptesService) {
